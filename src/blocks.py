f65c9870d0c2ca80250d6a81e4c5c7a1c60fe01c
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-05-02 14:42:31 +0200

    Added Image encoder for the discriminator

--- src/blocks.py
+++ src/blocks.py
@@ -1,28 +1,29 @@
 # TAGAN implementation
+from collections import OrderedDict
 
 import torch
-import torch.nn
+import torch.nn as nn
 
 
-class ConditioningAugmentation(torch.nn.Module):
+class ConditioningAugmentation(nn.Module):
     def forward(self, x):
         m = torch.mean(x)
         s = torch.std(x)
         return torch.randn(x.shape) * s + m
 
 
-class TemporalAverage(torch.nn.Module):
+class TemporalAverage(nn.Module):
     def forward(self, x):
         return x.mean()
 
 
-class TextEncoder(torch.nn.Module):
+class TextEncoder(nn.Module):
     def __init__(self, num_words):
         super().__init__()
-        self.main = torch.nn.Sequential(
-            torch.nn.GRU(input_size=num_words*300, hidden_size=512, bidirectional=True),
+        self.main = nn.Sequential(
+            nn.GRU(input_size=num_words*300, hidden_size=512, bidirectional=True),
             TemporalAverage(),
-            torch.nn.LeakyReLU(),
+            nn.LeakyReLU(),
             ConditioningAugmentation()
         )
 
@@ -30,44 +31,61 @@ class TextEncoder(torch.nn.Module):
         o = self.bgru.forward(text)
 
 
-class ImageEncoder(torch.nn.Module):
+class ImageEncoderDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 64, 4, 1, 2)),
+            ('act1', nn.LeakyReLU(0.2)),
+
+            ('conv2', nn.Conv2d(64, 128, 4, 1, 2)),
+            ('act2', nn.LeakyReLU(0.2)),
+
+            ('conv3', nn.Conv2d(128, 256, 4, 1, 2)),
+            ('act3', nn.LeakyReLU(0.2)),
+
+            ('conv4', nn.Conv2d(256, 512, 4, 1, 2)),
+            ('act4', nn.LeakyReLU(0.2)),
+
+            ('conv5', nn.Conv2d(512, 512, 4, 1, 2)),
+            ('act5', nn.LeakyReLU(0.2))]))
+
+    def forward(self, im):
+        return self.main.forward(im)
+
+
+class ConcatABResidualBlocks(nn.Module):
     pass
 
 
-class ConcatABResidualBlocks(torch.nn.Module):
+class ResidualBlock(nn.Module):
     pass
 
 
-class ResidualBlock(torch.nn.Module):
+class UnconditionalDiscriminator(nn.Module):
     pass
 
 
-class UnconditionalDiscriminator(torch.nn.Module):
+class Decoder(nn.Module):
     pass
 
 
-class Decoder(torch.nn.Module):
+class TextAdaptiveDiscriminator(nn.Module):
     pass
 
 
-class TextAdaptiveDiscriminator(torch.nn.Module):
+class ConditionalDiscriminator(nn.Module):
     pass
 
 
-class ConditionalDiscriminator(torch.nn.Module):
-    pass
-
-
-class Generator(torch.nn.Module):
+class Generator(nn.Module):
     def __init__(self, num_words):
         super().__init__()
-        self.main = torch.nn.Sequential(
-            TextEncoder(num_words),
-            ImageEncoder(),
-            ConcatABResidualBlocks(),
-            ResidualBlock(),
-            Decoder()
-        )
+        self.a = TextEncoder(num_words)
+        self.b = ImageEncoder()
+        self.ab = ConcatABResidualBlocks()
+        self.c = ResidualBlock()
+        self.d = Decoder()
 
     def forward(self, xtext, ximage):
         # x includes both the text and the image
@@ -79,10 +97,10 @@ class Generator(torch.nn.Module):
         return d
 
 
-class Discriminator(torch.nn.Module):
+class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ie = ImageEncoder()
+        self.ie = ImageEncoderDiscriminator()
         self.ud = UnconditionalDiscriminator()
         self.te = TextEncoder()
         self.tad = TextAdaptiveDiscriminator()
