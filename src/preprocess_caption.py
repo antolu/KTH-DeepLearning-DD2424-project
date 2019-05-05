7e46e602cd6044632f534850e9a0b3b6f6b03ef7
Anton Lu <antolu@kth.se>
2019-05-05 16:42:14 +0200

    zero-pad word vector for constant dimensions

--- src/preprocess_caption.py
+++ src/preprocess_caption.py
@@ -30,20 +30,28 @@ class PreprocessCaption :
         """
         return tokenizer.tokenize(sentence)
 
-    def string_to_vector(self, caption) :
+    def string_to_vector(self, caption, max_no_words=50) :
         """
         Converts captions to vector space representations using fasttext embeddings
         NOTE You have to execute the method `load_dataset` first, to load the fasttext embeddings
+
         :param The string, or list of strings to be transformed
-        :return A N by 300 matrix, where N is the number words in the caption 
+        :param max_no_words Maximum number of words in the caption, final tensor will be zero-padded to this number
+        :return A N by 300 matrix, where N is max_no_words. Also returns the number of actual words. 
         """
         if not self.fasttext_loaded :
             raise "Fasttext is not yet loaded. Use the method 'load_fasttext' to load the word embeddings first."
 
         tokens = self.__tokenize(caption)
+        no_tokens = len(tokens)
 
         word_vector = torch.Tensor([self.word_embedding.get_word_vector(word) for word in tokens])
 
-        return word_vector
+        # Pad the word vector so it always have the same dimensions
+        if no_tokens < max_no_words :
+            pad = torch.zeros(max_no_words - no_tokens, word_vector.shape[1])
+            word_vector = torch.cat((word_vector, pad))
+
+        return word_vector, no_tokens
 
         
