ae1500edae0a7404b19356b89c6c3a88993bce44
Anton Lu <antolu@kth.se>
2019-05-10 18:04:13 +0200

    update scripts to load generators

--- scripts/test_oxford.sh
+++ scripts/test_oxford.sh
@@ -4,6 +4,7 @@ python run_test.py --images_root $OXFORD_IMAGES_ROOT \
                     --annotations_root $OXFORD_CAPTIONS_ROOT \
                     --dataset oxford \
                     --fasttext_model $FASTTEXT_EMBEDDING \
-                    --pretrained_model $PRETRAINED_OXFORD_G \
+                    --pretrained_generator $PRETRAINED_OXFORD_G \
+                    --pretrained_discriminator $PRETRAINED_OXFORD_D \
                     --max_no_words $MAX_NO_WORDS  \
                     --runtype test
