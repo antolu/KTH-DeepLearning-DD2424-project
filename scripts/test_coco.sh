17029a093ab712d54717b610b2d913fd8e647566
Sergio Liberman <liberman@kth.se>
2019-05-15 20:56:45 +0000

    Updated coco test script

--- scripts/test_coco.sh
+++ scripts/test_coco.sh
@@ -4,9 +4,13 @@ python run_test.py --images_root $COCO_IMAGES_ROOT \
                     --annotations_root $COCO_CAPTIONS_ROOT \
                     --dataset coco \
                     --coco_set test \
-                    --keyword_file $COCO_KEYWORDS_FILE \
+                    --keywords_file $COCO_KEYWORDS_FILE \
                     --fasttext_model $FASTTEXT_EMBEDDING \
                     --pretrained_generator $PRETRAINED_COCO_G \
                     --pretrained_discriminator $PRETRAINED_COCO_D \
                     --max_no_words $MAX_NO_WORDS \
-                    --runtype test
+                    --blacklist $BLACKLIST \
+                    --runtype test \
+		    --no_epochs 600 \
+                    --pretrained_optimizer_discriminator $PRETRAINED_COCO_OD \
+                    --pretrained_optimizer_generator $PRETRAINED_COCO_OG
