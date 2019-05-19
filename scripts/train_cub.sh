2d6a63fd2e3895761ebc2b8da2888dd853ee4a2d
Sergio Liberman Bronfman <sliberman@alges.cl>
2019-05-19 18:33:58 +0200

    Modified script to fix bug

--- scripts/train_cub.sh
+++ scripts/train_cub.sh
@@ -6,7 +6,7 @@ python run.py --images_root $CUB_IMAGES_ROOT \
                     --fasttext_model $FASTTEXT_EMBEDDING \
                     --blacklist $BLACKLIST \
                     --max_no_words $MAX_NO_WORDS  \
-                    --runtype mode \
+                    --mode train \
                     --batch_size 64 \
                     --no_epochs 600
 #		    --pretrained_generator models/run_G_dataset_cub_before_dying.pth \
